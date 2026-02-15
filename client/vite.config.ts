202a1348f610a96cbe26dda4155ed88d9d732818
raomubasher5 <raomubasher55@gmail.com>
2026-02-16 00:22:57 +0500

    add host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server:{
+    host:true,
+    port : 5173
+  },
   resolve: {
     alias: {
       react: path.resolve(__dirname, 'node_modules/react'),
